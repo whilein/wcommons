dd53c2f9d7c8ab5cbbb376b9411dd2de0915382e
whilein <lero4ka6916@gmail.com>
2021-12-07 06:42:09 +0300

    feat(util): Iterables

---
+++ util/src/main/java/w/util/Iterables.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2021 Whilein
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package w.util;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+/**
+ * @author whilein
+ */
+@UtilityClass
+public class Iterables {
+
+    public <T> @NotNull Optional<@NotNull T> getFirst(final @NonNull Iterable<@Nullable T> iterable) {
+        val iterator = iterable.iterator();
+
+        return iterator.hasNext()
+                ? Optional.ofNullable(iterator.next())
+                : Optional.empty();
+    }
+
+}
