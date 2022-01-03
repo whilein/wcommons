665d0440a5d181b59865d18b509088ca490b82e8
whilein <lero4ka6916@gmail.com>
2022-01-04 01:37:40 +0300

    docs(annotation-index): add some javadocs

--- annotation-index/src/main/java/w/annotation/index/AnnotationIndex.java
+++ annotation-index/src/main/java/w/annotation/index/AnnotationIndex.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2021 Whilein
+ *    Copyright 2022 Whilein
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import w.util.ClassLoaderUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -48,28 +49,65 @@ public class AnnotationIndex {
         }
     }
 
+    /**
+     * Получить все имена классов, с аннотацией {@code annotation}
+     *
+     * @param cl         Загрузчик классов, из которого будет взят индекс аннотаций
+     * @param annotation Тип аннотации
+     * @return Список имён классов
+     */
     @SneakyThrows
-    private @NotNull List<@NotNull String> getAnnotatedTypeNames(final @NotNull ClassLoader cl, final @NotNull Class<?> annotation) {
+    public @NotNull List<@NotNull String> getAnnotatedTypeNames(
+            final @NotNull ClassLoader cl,
+            final @NotNull Class<? extends Annotation> annotation
+    ) {
         return _getAnnotated(cl, annotation.getName())
                 .collect(Collectors.toUnmodifiableList());
     }
 
+    /**
+     * Получить все классы, с аннотацией {@code annotation}
+     *
+     * @param cl         Загрузчик классов, из которого будет взят индекс аннотаций
+     * @param annotation Тип аннотации
+     * @return Список классов
+     */
     @SneakyThrows
-    public @NotNull List<@NotNull Class<?>> getAnnotatedTypes(final @NotNull ClassLoader cl, final @NotNull Class<?> annotation) {
+    public @NotNull List<@NotNull Class<?>> getAnnotatedTypes(
+            final @NotNull ClassLoader cl,
+            final @NotNull Class<? extends Annotation> annotation
+    ) {
         return _getAnnotated(cl, annotation.getName())
-                .map(ClassLoaderUtils::getClass)
+                .map(name -> ClassLoaderUtils.getClass(cl, name, false))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toUnmodifiableList());
     }
 
-
+    /**
+     * Получить все имена классов, с аннотацией {@code annotation}. Индекс аннотаций будет взят из
+     * класслоадера переданной аннотации.
+     *
+     * @param annotation Тип аннотации
+     * @return Список имён классов
+     */
     @SneakyThrows
-    private @NotNull List<@NotNull String> getAnnotatedTypeNames(final @NotNull Class<?> annotation) {
+    public @NotNull List<@NotNull String> getAnnotatedTypeNames(
+            final @NotNull Class<? extends Annotation> annotation
+    ) {
         return getAnnotatedTypeNames(annotation.getClassLoader(), annotation);
     }
 
+    /**
+     * Получить все классы, с аннотацией {@code annotation}. Индекс аннотаций будет взят из
+     * класслоадера переданной аннотации.
+     *
+     * @param annotation Тип аннотации
+     * @return Список классов
+     */
     @SneakyThrows
-    public @NotNull List<@NotNull Class<?>> getAnnotatedTypes(final @NotNull Class<?> annotation) {
+    public @NotNull List<@NotNull Class<?>> getAnnotatedTypes(
+            final @NotNull Class<? extends Annotation> annotation
+    ) {
         return getAnnotatedTypes(annotation.getClassLoader(), annotation);
     }
 
