6821cd0f90b2bf7eac33c194de56b960ff89e27d
whilein <lero4ka6916@gmail.com>
2022-01-05 19:27:58 +0300

    feat(util): suppress illegal access warning and java 17 support

--- util/src/main/java/w/util/Root.java
+++ util/src/main/java/w/util/Root.java
@@ -21,12 +21,16 @@ import lombok.experimental.UtilityClass;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import w.agent.AgentInstrumentation;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static java.lang.invoke.MethodType.methodType;
 
@@ -50,7 +54,7 @@ public class Root {
 
     private final MethodHandle UNSAFE__DEFINE_CLASS;
 
-    private final Method MODULE__IMPL_ADD_OPENS;
+    private final ModuleAccessor MODULE_ACCESSOR;
 
     static {
         MAGIC_ACCESSOR_IMPL_TYPE = ClassLoaderUtils.loadAny(
@@ -58,12 +62,11 @@ public class Root {
                 "jdk.internal.reflect.MagicAccessorImpl"
         );
 
-        try {
-            MODULE__IMPL_ADD_OPENS = Module.class.getDeclaredMethod("implAddOpens", String.class);
-            MODULE__IMPL_ADD_OPENS.setAccessible(true);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
+        val agentInstrumentationExists = ClassLoaderUtils.isClassAvailable("w.agent.AgentInstrumentation");
+
+        MODULE_ACCESSOR = agentInstrumentationExists
+                ? new AgentModuleAccessor()
+                : new SimpleModuleAccessor();
 
         openAccess(Root.class);
 
@@ -121,6 +124,16 @@ public class Root {
 
     @SneakyThrows
     public void openAccess(final @NotNull Class<?> type) {
+        MODULE_ACCESSOR.openAccess(type);
+    }
+
+    private interface ModuleAccessor {
+
+        void openAccess(@NotNull Class<?> type);
+
+    }
+
+    private static Set<Module> listBaseModules(final Class<?> type) {
         val base = type.getModule();
 
         val modules = new HashSet<Module>();
@@ -135,12 +148,59 @@ public class Root {
             modules.add(cl.getUnnamedModule());
         }
 
-        for (val module : modules) {
-            for (val name : module.getPackages()) {
-                MODULE__IMPL_ADD_OPENS.invoke(module, name);
-            }
-        }
+        return modules;
     }
 
+    private static final class SimpleModuleAccessor implements ModuleAccessor {
+
+        private static final Method MODULE__IMPL_ADD_OPENS;
+
+        static {
+            try {
+                MODULE__IMPL_ADD_OPENS = Module.class.getDeclaredMethod("implAddOpens", String.class);
+                MODULE__IMPL_ADD_OPENS.setAccessible(true);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        @SneakyThrows
+        public void openAccess(final @NotNull Class<?> type) {
+            for (val module : listBaseModules(type)) {
+                for (val name : module.getPackages()) {
+                    MODULE__IMPL_ADD_OPENS.invoke(module, name);
+                }
+            }
+        }
+
+    }
+
+    private static final class AgentModuleAccessor implements ModuleAccessor {
+
+        // спиздил у какого-то нн fiškа
+        @Override
+        public void openAccess(final @NotNull Class<?> type) {
+            val currentModule = Collections.singleton(type.getModule());
+
+            AgentInstrumentation.redefineModule(
+                    Object.class.getModule(),
+                    Collections.emptySet(),
+                    Map.of(
+                            "jdk.internal.misc", currentModule,
+                            "jdk.internal.loader", currentModule
+                    ),
+                    Map.of(
+                            "java.lang", currentModule,
+                            "java.lang.invoke", currentModule,
+                            "jdk.internal.misc", currentModule,
+                            "jdk.internal.loader", currentModule
+                    ),
+                    Collections.emptySet(),
+                    Collections.emptyMap()
+            );
+        }
+
+    }
 
 }
