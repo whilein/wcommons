9bf0f62e6057e2e4643cf6e44a1ef0ff08dc4889
whilein <lero4ka6916@gmail.com>
2022-02-03 07:55:22 +0300

    feat: Root#isUnsafeSupported

--- util/src/main/java/w/util/Root.java
+++ util/src/main/java/w/util/Root.java
@@ -25,12 +25,10 @@ import w.agent.AgentInstrumentation;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import static java.lang.invoke.MethodType.methodType;
 
@@ -40,23 +38,16 @@ import static java.lang.invoke.MethodType.methodType;
 @UtilityClass
 public class Root {
 
-    private final MethodHandles.Lookup IMPL_LOOKUP;
-
+    /**
+     * Класс {@code MagicAccessorImpl}
+     */
+    public final Class<?> MAGIC_ACCESSOR_IMPL_TYPE;
     /**
      * Класс {@code Unsafe}
      */
     public final Class<?> UNSAFE_TYPE;
 
-    /**
-     * Класс {@code MagicAccessorImpl}
-     */
-    public final Class<?> MAGIC_ACCESSOR_IMPL_TYPE;
-
-    private final MethodHandle UNSAFE__DEFINE_CLASS;
-
-    private final MethodHandle UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY;
-
-    private final ModuleAccessor MODULE_ACCESSOR;
+    private final Unsafe UNSAFE;
 
     static {
         MAGIC_ACCESSOR_IMPL_TYPE = ClassLoaderUtils.loadAny(
@@ -64,49 +55,30 @@ public class Root {
                 "jdk.internal.reflect.MagicAccessorImpl"
         );
 
-        val agentInstrumentationExists = ClassLoaderUtils.isClassAvailable("w.agent.AgentInstrumentation");
+        UNSAFE_TYPE = ClassLoaderUtils.getClass("jdk.internal.misc.Unsafe");
 
-        MODULE_ACCESSOR = agentInstrumentationExists
-                ? new AgentModuleAccessor()
-                : new SimpleModuleAccessor();
+        Unsafe unsafe;
 
-        openAccess(Root.class);
-
-        // region IMPL_LOOKUP
         try {
-            val field = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-            field.setAccessible(true);
+            val agentInstrumentationExists = ClassLoaderUtils.isClassAvailable(
+                    "w.agent.AgentInstrumentation");
 
-            if ((IMPL_LOOKUP = (MethodHandles.Lookup) field.get(null)) == null) {
-                throw new IllegalStateException("Lookup.IMPL_LOOKUP is null");
+            if (agentInstrumentationExists) {
+                openAccessWithAgent();
+            } else {
+                openAccessWithReflection();
             }
+
+            unsafe = new UnsafeImpl();
         } catch (final Exception e) {
-            throw new RuntimeException(e);
+            unsafe = new UnsafeStub();
         }
-        // endregion
 
-        // region Unsafe
-        try {
-            UNSAFE_TYPE = Class.forName("jdk.internal.misc.Unsafe");
+        UNSAFE = unsafe;
+    }
 
-            val theUnsafe = IMPL_LOOKUP.findStaticVarHandle(UNSAFE_TYPE, "theUnsafe",
-                    UNSAFE_TYPE).get();
-
-            UNSAFE__DEFINE_CLASS = IMPL_LOOKUP.findVirtual(UNSAFE_TYPE, "defineClass",
-                            methodType(Class.class, String.class, byte[].class, int.class, int.class,
-                                    ClassLoader.class, ProtectionDomain.class))
-                    .bindTo(theUnsafe);
-
-            UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY = IMPL_LOOKUP.findVirtual(
-                            UNSAFE_TYPE,
-                            "allocateUninitializedArray",
-                            methodType(Object.class, Class.class, int.class)
-                    )
-                    .bindTo(theUnsafe);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-        // endregion
+    public boolean isUnsafeSupported() {
+        return UNSAFE.isSupported();
     }
 
     @SneakyThrows
@@ -118,102 +90,213 @@ public class Root {
             final @Nullable ClassLoader classLoader,
             final @Nullable ProtectionDomain protectionDomain
     ) {
-        return (Class<?>) UNSAFE__DEFINE_CLASS.invokeExact(name, data,
-                offset, length, classLoader, protectionDomain);
+        return UNSAFE.defineClass(name, data, offset, length, classLoader, protectionDomain);
     }
 
-    @SneakyThrows
     public @NotNull Object allocateUninitializedArray(final Class<?> componentType, final int length) {
-        return UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY.invokeExact(componentType, length);
+        return UNSAFE.allocateUninitializedArray(componentType, length);
     }
 
     public @NotNull MethodHandles.Lookup trustedLookup() {
-        return IMPL_LOOKUP;
+        return UNSAFE.trustedLookup();
     }
 
     public @NotNull MethodHandles.Lookup trustedLookupIn(final @NotNull Class<?> type) {
-        return IMPL_LOOKUP.in(type);
+        return UNSAFE.trustedLookupIn(type);
     }
 
-    @SneakyThrows
-    public void openAccess(final @NotNull Class<?> type) {
-        MODULE_ACCESSOR.openAccess(type);
-    }
+    private static final class UnsafeImpl extends Unsafe {
 
-    private interface ModuleAccessor {
+        private static final MethodHandles.Lookup IMPL_LOOKUP;
 
-        void openAccess(@NotNull Class<?> type);
+        private static final MethodHandle UNSAFE__DEFINE_CLASS;
 
-    }
-
-    private static Set<Module> listBaseModules(final Class<?> type) {
-        val base = type.getModule();
-
-        val modules = new HashSet<Module>();
-
-        if (base.getLayer() != null) {
-            modules.addAll(base.getLayer().modules());
-        }
-
-        modules.addAll(ModuleLayer.boot().modules());
-
-        for (ClassLoader cl = type.getClassLoader(); cl != null; cl = cl.getParent()) {
-            modules.add(cl.getUnnamedModule());
-        }
-
-        return modules;
-    }
-
-    private static final class SimpleModuleAccessor implements ModuleAccessor {
-
-        private static final Method MODULE__IMPL_ADD_OPENS;
+        private static final MethodHandle UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY;
 
         static {
+            // region IMPL_LOOKUP
             try {
-                MODULE__IMPL_ADD_OPENS = Module.class.getDeclaredMethod("implAddOpens", String.class);
-                MODULE__IMPL_ADD_OPENS.setAccessible(true);
+                val field = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+                field.setAccessible(true);
+
+                if ((IMPL_LOOKUP = (MethodHandles.Lookup) field.get(null)) == null) {
+                    throw new IllegalStateException("Lookup.IMPL_LOOKUP is null");
+                }
             } catch (final Exception e) {
                 throw new RuntimeException(e);
             }
+            // endregion
+
+            // region Unsafe
+            try {
+                val theUnsafe = IMPL_LOOKUP.findStaticVarHandle(UNSAFE_TYPE, "theUnsafe",
+                        UNSAFE_TYPE).get();
+
+                UNSAFE__DEFINE_CLASS = IMPL_LOOKUP.findVirtual(UNSAFE_TYPE, "defineClass",
+                                methodType(Class.class, String.class, byte[].class, int.class, int.class,
+                                        ClassLoader.class, ProtectionDomain.class))
+                        .bindTo(theUnsafe);
+
+                UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY = IMPL_LOOKUP.findVirtual(
+                                UNSAFE_TYPE,
+                                "allocateUninitializedArray",
+                                methodType(Object.class, Class.class, int.class)
+                        )
+                        .bindTo(theUnsafe);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+            // endregion
+        }
+
+        @Override
+        public MethodHandles.Lookup trustedLookupIn(final Class<?> type) {
+            return IMPL_LOOKUP.in(type);
+        }
+
+        @Override
+        public MethodHandles.Lookup trustedLookup() {
+            return IMPL_LOOKUP;
         }
 
         @Override
         @SneakyThrows
-        public void openAccess(final @NotNull Class<?> type) {
-            for (val module : listBaseModules(type)) {
-                for (val name : module.getPackages()) {
-                    MODULE__IMPL_ADD_OPENS.invoke(module, name);
-                }
-            }
+        public Class<?> defineClass(
+                final String name,
+                final byte[] data,
+                final int offset, final int length,
+                final ClassLoader classLoader,
+                final ProtectionDomain protectionDomain
+        ) {
+            return (Class<?>) UNSAFE__DEFINE_CLASS.invokeExact(name, data,
+                    offset, length, classLoader, protectionDomain);
+        }
+
+        @Override
+        @SneakyThrows
+        public Object allocateUninitializedArray(final Class<?> componentType, final int length) {
+            return UNSAFE__ALLOCATE_UNINITIALIZED_ARRAY.invokeExact(componentType, length);
+        }
+
+
+        @Override
+        public boolean isSupported() {
+            return true;
+        }
+    }
+
+    private static final class UnsafeStub extends Unsafe {
+
+        private UnsupportedOperationException constructException() {
+            return new UnsupportedOperationException(
+                    "Root is not available on current JVM version. " +
+                    "To fix the issue, add io.github.whilein.wcommons:agent to dependencies"
+            );
+        }
+
+        @Override
+        public MethodHandles.Lookup trustedLookupIn(final Class<?> type) {
+            throw constructException();
+        }
+
+        @Override
+        public MethodHandles.Lookup trustedLookup() {
+            throw constructException();
+        }
+
+        @Override
+        public Class<?> defineClass(
+                final String name,
+                final byte[] data,
+                final int offset, final int length,
+                final ClassLoader classLoader,
+                final ProtectionDomain protectionDomain
+        ) {
+            throw constructException();
+        }
+
+        @Override
+        public Object allocateUninitializedArray(final Class<?> componentType, final int length) {
+            throw constructException();
+        }
+
+        @Override
+        public boolean isSupported() {
+            return false;
         }
 
     }
 
-    private static final class AgentModuleAccessor implements ModuleAccessor {
+    private static abstract class Unsafe {
 
-        // спиздил у какого-то нн fiškа
-        @Override
-        public void openAccess(final @NotNull Class<?> type) {
-            val currentModule = Collections.singleton(type.getModule());
+        public abstract boolean isSupported();
 
-            AgentInstrumentation.redefineModule(
-                    Object.class.getModule(),
-                    Collections.emptySet(),
-                    Map.of(
-                            "jdk.internal.misc", currentModule,
-                            "jdk.internal.loader", currentModule
-                    ),
-                    Map.of(
-                            "java.lang", currentModule,
-                            "java.lang.invoke", currentModule,
-                            "jdk.internal.misc", currentModule,
-                            "jdk.internal.loader", currentModule
-                    ),
-                    Collections.emptySet(),
-                    Collections.emptyMap()
-            );
+        public abstract MethodHandles.Lookup trustedLookupIn(Class<?> type);
+
+        public abstract MethodHandles.Lookup trustedLookup();
+
+        public abstract Class<?> defineClass(
+                String name,
+                byte[] data,
+                int offset,
+                int length,
+                ClassLoader classLoader,
+                ProtectionDomain protectionDomain
+        );
+
+        public abstract Object allocateUninitializedArray(Class<?> componentType, int length);
+
+    }
+
+    @SneakyThrows
+    private static void openAccessWithReflection() {
+        val modules = new HashSet<Module>();
+
+        {
+            val type = Root.class;
+
+            val base = type.getModule();
+
+            if (base.getLayer() != null) {
+                modules.addAll(base.getLayer().modules());
+            }
+
+            modules.addAll(ModuleLayer.boot().modules());
+
+            for (ClassLoader cl = type.getClassLoader(); cl != null; cl = cl.getParent()) {
+                modules.add(cl.getUnnamedModule());
+            }
         }
 
+        val method = Module.class.getDeclaredMethod("implAddOpens", String.class);
+        method.setAccessible(true);
+
+        for (val module : modules) {
+            for (val name : module.getPackages()) {
+                method.invoke(module, name);
+            }
+        }
+    }
+
+    private static void openAccessWithAgent() {
+        val currentModule = Collections.singleton(Root.class.getModule());
+
+        AgentInstrumentation.redefineModule(
+                Object.class.getModule(),
+                Collections.emptySet(),
+                Map.of(
+                        "jdk.internal.misc", currentModule,
+                        "jdk.internal.loader", currentModule
+                ),
+                Map.of(
+                        "java.lang", currentModule,
+                        "java.lang.invoke", currentModule,
+                        "jdk.internal.misc", currentModule,
+                        "jdk.internal.loader", currentModule
+                ),
+                Collections.emptySet(),
+                Collections.emptyMap()
+        );
     }
 
 }
